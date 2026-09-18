a42989547977b15f8474920ec2fde353f1c6b544
mugishad773 <mugishad773@gmail.com>
2026-09-18 13:36:53 +0200

    Fix PWA Cache locking the Android WebView

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'directcall-cache-v1';
+const CACHE_NAME = 'directcall-cache-v3';
 const urlsToCache = [
     './',
     './index.html',
@@ -19,5 +19,19 @@ self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
