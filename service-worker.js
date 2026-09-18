fc62eb0b4e9f37bf6b158ed76e27d887bccf6990
mugishad773 <mugishad773@gmail.com>
2026-09-18 15:09:07 +0200

    Force service worker cache bypass

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'directcall-cache-v4';
+const CACHE_NAME = 'directcall-cache-v5';
 const urlsToCache = [
     './',
     './index.html',
@@ -9,6 +9,7 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(urlsToCache))
@@ -32,6 +33,6 @@ self.addEventListener('activate', event => {
                     }
                 })
             );
-        })
+        }).then(() => self.clients.claim())
     );
 });
